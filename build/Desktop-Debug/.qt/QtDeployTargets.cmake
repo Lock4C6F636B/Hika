02f50381037a61e89d2ab47e0c5eefe17ea0221b
Lock4C6F636B <traiser@email.cz>
2025-05-25 21:33:28 +0900

    adding audio

--- build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_HiKa_FILE /home/kasumi/programming/c++/HiKa_linux/build/Desktop-Debug/HiKa)
-set(__QT_DEPLOY_TARGET_HiKa_TYPE EXECUTABLE)
--- build/Desktop-Debug/CMakeFiles/HiKa.dir/HiKa_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/HiKa.dir/HiKa_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /home/kasumi/programming/c++/HiKa_linux/build/Desktop-Debug/HiKa_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
